c4eddda181825a104e92965ab92b959d89fd58f4
Will Eardley <eardley.will@gmail.com>
2025-07-23 21:15:26 +0100

    Fix background image paths in ThreePointJumbo component for correct asset loading

--- client/src/components/3Points-Jumbo.jsx
+++ client/src/components/3Points-Jumbo.jsx
@@ -6,25 +6,26 @@ import '../Fonts.css'; // Import custom font styles
 import '../ThreePoints.css'; // New CSS for background images
 
 
+
 function ThreePointJumbo() {
     return (
         <Container fluid id="ThreePoints" className="py-5 text-center">
             <Row className="justify-content-center">
     
             <Col md={3} className="mb-4 feature-block">
-                <div className="feature-icon" style={{ backgroundImage: `url('/Pelago-Favicon.svg')`, color: 'black' }}></div>
+                <div className="feature-icon" style={{ backgroundImage: `url('../assets/Pelago-Favicon.svg')`, color: 'black' }}></div>
                 <h1 style={{ fontFamily: 'Sailor Condensed' }}>Eco-Friendly</h1>
                 <p style={{ fontFamily: 'Sailor Condensed Italic' }}>No printing, No Waste</p>
             </Col>
     
             <Col md={3} className="mb-4 feature-block">
-                <div className="feature-icon" style={{ backgroundImage: `url('/Pelago-Favicon.svg')` }}></div>
+                <div className="feature-icon" style={{ backgroundImage: `url('../assets/Pelago-Favicon.svg')` }}></div>
                 <h1 style={{ fontFamily: 'Sailor Condensed' }}>Always Up To Date</h1>
                 <p style={{ fontFamily: 'Sailor Condensed Italic' }}>Update details in an instant</p>
             </Col>
     
             <Col md={3} className="mb-4 feature-block">
-                <div className="feature-icon" style={{ backgroundImage: `url('/Pelago-Favicon.svg')` }}></div>
+                <div className="feature-icon" style={{ backgroundImage: `url('../assets/Pelago-Favicon.svg')` }}></div>
                 <h1 style={{ fontFamily: 'Sailor Condensed' }}>Instant Sharing</h1>
                 <p style={{ fontFamily: 'Sailor Condensed Italic' }}>Share with a tap or a scan</p>
             </Col>
