81c6c802127bbdc7ecc862ffa36c082b7b95bd89
Will Eardley <eardley.will@gmail.com>
2025-07-23 20:29:36 +0100

    Update index.html and 3Points-Jumbo component for improved styling and functionality; adjust Bootstrap version and enhance eco-friendly feature display.

--- client/src/components/3Points-Jumbo.jsx
+++ client/src/components/3Points-Jumbo.jsx
@@ -6,15 +6,13 @@ import '../Fonts.css'; // Import custom font styles
 import '../ThreePoints.css'; // New CSS for background images
 
 
-
-
 function ThreePointJumbo() {
     return (
         <Container fluid id="ThreePoints" className="py-5 text-center">
             <Row className="justify-content-center">
     
             <Col md={3} className="mb-4 feature-block">
-                <div className="feature-icon" style={{ backgroundImage: `url('/Pelago-Favicon.svg')` }}></div>
+                <div className="feature-icon" style={{ backgroundImage: `url('/Pelago-Favicon.svg')`, color: 'black' }}></div>
                 <h1 style={{ fontFamily: 'Sailor Condensed' }}>Eco-Friendly</h1>
                 <p style={{ fontFamily: 'Sailor Condensed Italic' }}>No printing, No Waste</p>
             </Col>
