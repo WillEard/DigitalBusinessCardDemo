b8d8e7b03a9f82f1280b516f132761b3539a5e39
Will Eardley <eardley.will@gmail.com>
2025-07-23 21:18:19 +0100

    Remove inline background images from feature icons in ThreePointJumbo component

--- client/src/components/3Points-Jumbo.jsx
+++ client/src/components/3Points-Jumbo.jsx
@@ -13,19 +13,19 @@ function ThreePointJumbo() {
             <Row className="justify-content-center">
     
             <Col md={3} className="mb-4 feature-block">
-                <div className="feature-icon" style={{ backgroundImage: `url('../assets/Pelago-Favicon.svg')`, color: 'black' }}></div>
+                <div className="feature-icon" ></div>
                 <h1 style={{ fontFamily: 'Sailor Condensed' }}>Eco-Friendly</h1>
                 <p style={{ fontFamily: 'Sailor Condensed Italic' }}>No printing, No Waste</p>
             </Col>
     
             <Col md={3} className="mb-4 feature-block">
-                <div className="feature-icon" style={{ backgroundImage: `url('../assets/Pelago-Favicon.svg')` }}></div>
+                <div className="feature-icon" ></div>
                 <h1 style={{ fontFamily: 'Sailor Condensed' }}>Always Up To Date</h1>
                 <p style={{ fontFamily: 'Sailor Condensed Italic' }}>Update details in an instant</p>
             </Col>
     
             <Col md={3} className="mb-4 feature-block">
-                <div className="feature-icon" style={{ backgroundImage: `url('../assets/Pelago-Favicon.svg')` }}></div>
+                <div className="feature-icon" ></div>
                 <h1 style={{ fontFamily: 'Sailor Condensed' }}>Instant Sharing</h1>
                 <p style={{ fontFamily: 'Sailor Condensed Italic' }}>Share with a tap or a scan</p>
             </Col>
