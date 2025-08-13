8ae2c338d95723e32c5f7014aca41c5be4af0dc1
Will Eardley <eardley.will@gmail.com>
2025-08-13 22:02:28 +0100

    feat: Add PropTypes and defaultProps to Banner component for improved type checking and default values

--- client/src/components/NavBanner.jsx
+++ client/src/components/NavBanner.jsx
@@ -1,6 +1,8 @@
 // Styles
 import '../styles/Banner.css';
 
+import PropTypes from 'prop-types';
+
 const Banner = ({ message = "Use code 'Conserve' for 5% off your first month", onClose }) => {
   return (
     <div className="alert-banner text-center text-white px-3 py-2 d-flex justify-content-center align-items-center">
@@ -12,6 +14,16 @@ const Banner = ({ message = "Use code 'Conserve' for 5% off your first month", o
       )}
     </div>
   );
+};
+
+Banner.propTypes = {
+  message: PropTypes.string,        // message should be a string
+  onClose: PropTypes.func,          // onClose should be a function if provided
+};
+
+Banner.defaultProps = {
+  message: "Use code 'Conserve' for 5% off your first month", // default message
+  onClose: null,                   // default to null if no onClose function is provided
 };
 
 export default Banner;
